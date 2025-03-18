9777e30e30391bf8e52f2335461df1501e756649
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-03-18 09:59:32 +0900

    express 기본 예제

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// http 모듈을 가져옵니다
-const http = require('http');
+// Express 모듈을 불러오기
+const express = require('express');
 
-// 서버를 만듭니다
-const server = http.createServer((req, res) => {
-  // 응답 헤더를 설정합니다
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+//Express 에플리케이션을 생성
+const app = express();
 
-  // 클라이언트에게 보낼 응답 메시지를 작성합니다
-  res.end('Hello, World!\n');
-});
+//라우팅 설정
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
 
-// 서버를 3000번 포트에서 실행합니다
-// localhost를 쓸 수 있는 이유 : DNS가 도메인 이름을 ip주소로 변환해주기 때문
-server.listen(3000, () => {
+// 서버를 3000번 포트에서 실행
+app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
