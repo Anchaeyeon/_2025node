c3fe9826c6f18d4aa94d46903ec68fdd43afb95e
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-04-02 14:10:45 +0900

    body-parser 신버전

--- practice/body-parser.js
+++ practice/body-parser.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-//express안에 body-parser가 내장되어 있어서 따로 다운하지 않아도 됨
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.post('/submit', (req, res) => {
   const {name, year} = req.body;
@@ -14,4 +12,4 @@ app.post('/submit', (req, res) => {
 
 app.listen(3001, () => {
   console.log('서버가 http://localhost:3001 에서 실행 중입니다.');
-});
+}); 
